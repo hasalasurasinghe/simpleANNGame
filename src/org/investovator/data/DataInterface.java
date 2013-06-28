faf5d0a1efec513118d9aeaac29d9a9653566b6a
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-06-28 22:38:06 +0530

    added the data interface

---
+++ src/org/investovator/data/DataInterface.java
@@ -0,0 +1,18 @@
+package org.investovator.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ishan
+ * Date: 6/28/13
+ * Time: 10:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DataInterface {
+
+    /**
+     * Parameters : Company, enum values for the data items, number of rows needed, start date
+     *
+     * @return
+     */
+    public float[][] getData();
+}
