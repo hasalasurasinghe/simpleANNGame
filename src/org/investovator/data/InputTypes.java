cfa8a0cd053c4998f76027687f1cd6125ca64dfb
Amila Surendra <amilasurendra@gmail.com>
2013-06-29 13:47:51 +0530

    Added input data types enum

---
+++ src/org/investovator/data/InputTypes.java
@@ -0,0 +1,17 @@
+package org.investovator.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amila
+ * Date: 6/29/13
+ * Time: 1:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum InputTypes {
+    DATE,
+    STARTING_PRICE,
+    CLOSING_PRICE,
+    LOW_PRICE,
+    HIGH_PRICE,
+    SHARES_TRADED
+}
