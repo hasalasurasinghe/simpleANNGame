7e41169d4d332725f14843f595c8749acace91b7
Amila Surendra <amilasurendra@gmail.com>
2013-06-30 10:44:20 +0530

    added simple normalization algorithm

--- src/main/java/org/investovator/neuralNet/NormalizationModel.java
+++ src/main/java/org/investovator/neuralNet/NormalizationModel.java
@@ -16,13 +16,25 @@ public class NormalizationModel {
     private float oldMin;
     private float newMin;
 
-    public float getNormalizedValue(float data){
+    /*
+        MinMax normalization formula
 
-        throw new NotImplementedException();
+        out = (in - min)/(max - min)*(new_max-new_min) + new_min
+
+     */
+
+    public NormalizationModel(float newMax, float oldMax, float oldMin, float newMin) {
+        this.newMax = newMax;
+        this.oldMax = oldMax;
+        this.oldMin = oldMin;
+        this.newMin = newMin;
+    }
+
+    public float getNormalizedValue(float data){
+         return (data - oldMin)/(oldMax - oldMin)*(newMax-newMin) + newMax;
     }
 
     public float getDenormalizedValue(float normalizedValue) {
-
-        throw new NotImplementedException();
+        return (normalizedValue - newMin)*(oldMax - oldMin)/(newMax-newMin) + oldMin;
     }
 }
